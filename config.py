6e2e2cdca11e038e100dce99fba97e0a8ac969bd
Richard Schoen <richard@richardschoen.net>
2024-04-28 16:11:27 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Flask config file
+# https://pythonise.com/series/learning-flask/flask-configuration-files
+# Put system config settings in here and include config.py in your apps
+
+[settings]
+mockfiledirectory=/mockjson
+debug=1
+allowrawfiles=1
+contenttyperaw=application/text
